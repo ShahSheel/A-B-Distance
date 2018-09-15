5617ca2b4da9a157ea690f07ac971e012079ce2c
Sheel Shah <13870434+ShahSheel@users.noreply.github.com>
2018-09-15 22:41:19 +0100

    hey

---
+++ searchCriteria.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+      document.getElementById('Submit').addEventListener('click', url);      
+});
+
+
+function url(){
+    if (document.getElementById('location') != null) {
+    var location = document.getElementById("location").value;
+  }
+
+    if (document.getElementById('destination') != null) {
+    var destination = document.getElementById("destination").value;
+  }
+
+    var beginURL = "http://maps.google.com/maps?saddr=";
+    var endURL = "&daddr=";
+    url = beginURL.concat(location,endURL,destination);
+    openInNewTab(url);
+}
+
+function openInNewTab(url) {
+  var win = window.open(url, '_blank');
+  win.focus();
+}
