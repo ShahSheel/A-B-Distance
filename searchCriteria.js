22552cc90a47945ad7ba20a20a77c5e5741df4b7
Sheel Shah <13870434+ShahSheel@users.noreply.github.com>
2018-09-16 22:31:37 +0100

    Added Ajax method, doesnt work properly

--- searchCriteria.js
+++ searchCriteria.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-      document.getElementById('Submit').addEventListener('click',url);      
+      document.getElementById('Submit').addEventListener('click',init);      
 });
-
-var fullurl;
+var mainURL;
+function init(){
+  mainURL = url();
+  callAjax();
+}
 function url(){
     if (document.getElementById('location') != null) {
     var location = document.getElementById("location").value;
@@ -23,28 +26,29 @@ function url(){
     var addDestnation = "&to="
     var addDest = addDestnation.concat(destination);
     url2 = url.concat(addDest);
-    var returnXMLFormat = "&outFormat=JSON&ambiguities=ignore&"
+    var returnXMLFormat = "&outFormat=XML&ambiguities=ignore&"
     url3 = url2.concat(returnXMLFormat);
     var routeType = "routeType=";
     var combineRoute = routeType.concat(selection);
     url4 = url3.concat(combineRoute);
     var endURL = "&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false"
     finalURL = url4.concat(endURL);
-    fullurl = finalURL;
-    callAjax(fullurl);
+    return "https://www.mapquestapi.com/directions/v2/route?key=9vyfTMs7Q4JVtr4u5ItN8Fd0YHuw1dqn&from=Cr44ef&to=horsham&outFormat=XML&ambiguities=ignore&routeType=shortest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false";
 }
-
- $.ajax({
-  type: "GET",
-  url: "https://www.mapquestapi.com/directions/v2/route?key=9vyfTMs7Q4JVtr4u5ItN8Fd0YHuw1dqn&from=Cr44ef&to=horsham&outFormat=XML&ambiguities=ignore&routeType=shortest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false",
-  dataType: "xml",
-  success: parseXML
-});
-
+/** currently this doens't work. 
+returns Object object when having a function, else works without it..but it would be called first rather than from init() 
+**/
+function callAjax(){
+   $.ajax({
+    type: "GET",
+    url: mainURL,
+    dataType: "XML",
+    success: parseXML,
+  });
+}
 
 function parseXML(xml) {
   $('#xmlSource').val( xmlToString(xml) );
-  
   var ballList = $('distance', xml).get();
   $.each(ballList, function( index, value ) {
     console.log( index + ": " + value.textContent );
@@ -63,7 +67,6 @@ function xmlToString(xmlData) {
   }
   return xmlString;
 }  
-
 
 
 
--- test.js
+++ test.js
@@ -1,26 +1,21 @@
-
-$.ajax({
-  type: "GET",
-  url: "https://www.mapquestapi.com/directions/v2/route?key=9vyfTMs7Q4JVtr4u5ItN8Fd0YHuw1dqn&from=Cr44ef&to=horsham&outFormat=XML&ambiguities=ignore&routeType=shortest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false",
-  dataType: "xml",
-  success: parseXML
-});
-
-
+function getAjax(mainURL){
+   $.ajax({
+    type: "GET",
+    async: false,
+    url: mainURL,
+    dataType: "xml",
+    success: parseXML
+  });
+}
 
 function parseXML(xml) {
-  alert("Got here");
   $('#xmlSource').val( xmlToString(xml) );
   
   var ballList = $('distance', xml).get();
-  var ballCount=0;
   $.each(ballList, function( index, value ) {
     console.log( index + ": " + value.textContent );
-    $('#results').append(value.textContent);
-    if (ballCount < ballList.length-1) {
-      $('#results').append(",");
-    }
-    ballCount++;
+    $('#r_distance').append(value.textContent);
+    return false;
   });
   
 }
