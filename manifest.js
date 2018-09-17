026c1842e75863e283d272a3deef0dd1f7dd13ed
Sheel Shah <13870434+ShahSheel@users.noreply.github.com>
2018-09-17 21:56:57 +0100

    Deleted unwanted files

--- manifest.js
+++
@@ -1,6 +0,0 @@
-  {
-    "name": "QuickLocation",
-    "version": "1.0",
-    "description": "Find distance between point A to point B without needing to open another tab",
-    "manifest_version": 2
-  }
