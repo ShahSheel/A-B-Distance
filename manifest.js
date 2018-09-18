e7d531dcc8404325d3f636ac1f708010c8ce8fb7
Sheel Shah <13870434+ShahSheel@users.noreply.github.com>
2018-09-18 22:33:18 +0100

    Daily push

---
+++ manifest.js
@@ -0,0 +1,6 @@
+  {
+    "name": "QuickLocation",
+    "version": "1.0",
+    "description": "Find distance between point A to point B without needing to open another tab",
+    "manifest_version": 2
+  }
